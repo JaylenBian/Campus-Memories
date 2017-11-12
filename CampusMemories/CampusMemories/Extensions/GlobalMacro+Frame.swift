ac94dc4c72f8e0034915959276d7db24228d1125
JunLin Bian <Minecoder@163.com>
2017-11-12 13:04:10 +0800

    添加项目Extension

---
+++ CampusMemories/CampusMemories/Extensions/GlobalMacro+Frame.swift
@@ -0,0 +1,19 @@
+//
+//  GlobalMacro+Frame.swift
+//  CampusMemories
+//
+//  Created by Minecode on 2017/11/12.
+//  Copyright © 2017年 Minecode. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - Size Defination
+
+// Screen Defination
+let kScreenWidth = UIScreen.main.bounds.width
+let kScreenHeight = UIScreen.main.bounds.height
+
+
+// View Defination
---
+++ CampusMemories/CampusMemories/Extensions/GlobalMacro+URL.swift
@@ -0,0 +1,12 @@
+//
+//  GlobalMacro+URL.swift
+//  CampusMemories
+//
+//  Created by Minecode on 2017/11/12.
+//  Copyright © 2017年 Minecode. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - URL Defination
+
