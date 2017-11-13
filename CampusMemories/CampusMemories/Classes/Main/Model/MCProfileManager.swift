d3832bf6e982a99e33ee20bbcae63d1179f952cf
JunLin Bian <Minecoder@163.com>
2017-11-13 22:59:50 +0800

    添加用户信息管理类

---
+++ CampusMemories/CampusMemories/Classes/Main/Model/MCProfileManager.swift
@@ -0,0 +1,21 @@
+//
+//  MCProfileManager.swift
+//  CampusMemories
+//
+//  Created by Minecode on 2017/11/13.
+//  Copyright © 2017年 Minecode. All rights reserved.
+//
+
+import UIKit
+
+class MCProfileManager {
+    
+    // 使用单例模式管理用户信息
+    static let sharedInstance = MCProfileManager.init()
+    
+    
+    private init() {
+        
+    }
+    
+}
---
+++ CampusMemories/CampusMemories/Classes/Main/Model/MCUserProfile.swift
@@ -0,0 +1,40 @@
+//
+//  MCUserProfile.swift
+//  CampusMemories
+//
+//  Created by Minecode on 2017/11/13.
+//  Copyright © 2017年 Minecode. All rights reserved.
+//
+
+import UIKit
+
+class MCUserProfile: NSObject {
+    
+    var id: Int?
+    var name: String?
+    var pass: String?
+    var sign: String?
+    var sex: String?
+    var blogcount: Int?
+    var school: String?
+    var age: Int?
+    var qq: String?
+    var tel: String?
+    var number: String?
+    var nick: String?
+    var fanscount: Int?
+    var uptime: String?
+    var face: String?
+    var sid: String?
+    
+    init(dict: [String: Any]) {
+        super.init()
+        
+        self.setValuesForKeys(dict)
+    }
+    
+    override func setValue(_ value: Any?, forUndefinedKey key: String) {
+        // 占位
+    }
+    
+}
